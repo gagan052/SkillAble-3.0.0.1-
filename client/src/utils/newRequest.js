6387063d6e647da92d5813f228b09daa925129b8
gagan052 <gagansaini7207@gmail.com>
2025-07-15 11:22:10 +0530

    Enhance error handling in API requests

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://skillable-3-0-0-1.onrender.com/api/",
   withCredentials: true,
 });
 
